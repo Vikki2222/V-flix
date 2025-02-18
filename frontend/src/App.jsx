b53c04df887ec7e2474a12a4f14d8d4f6edb966c
Vikki2222 <vigneshp18189@gmail.com>
2025-02-18 21:53:51 +0530

    from feb18

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import MovieCard from './components/MovieCard';
 
 function App() {
-  
-
-  return (<>
+  return (
+  <>
+  <MovieCard movie={{title:"Viks film",release_date:"2024"}}></MovieCard>
+  <MovieCard movie={{title:"Vik film",release_date:"2020"}}></MovieCard>
   </>
   );
 }
